60ae13ff67b2e436fff2f9e8e840216b5d426ee6
ashish-adishtech <ashish@adistech.in>
2023-01-07 11:15:22 +0530

    7-1form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularProject';
+  // demoData = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   email: new FormControl(''),
+  // });
+
+  onSubmit(item:any){
+    console.log(item.form.value)
+  }
+
 }
